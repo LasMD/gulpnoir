d4acb6043f91994795b2042eaa7ce9c4de0ad317
Wyatt Arent <hello@wyatt.ninja>
2016-07-21 18:28:12 -0600

    Allow adding plugins to added plugin list. Update menubar

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import NavButton from '../../components/NavButton';
+import { Button } from 'react-toolbox/lib/button';
 import '!style!css!sass!./_style.scss';
 import $ from 'jquery';
 
 export default class HomePage extends Component {
 
   componentDidMount() {
+
+    this.setState({addedPlugins: []});
+
     $.get('http://npmsearch.com/query?fields=name,keywords,rating,description,author,modified,homepage,version&q=keywords:gulpfriendly&q=keywords:gulpplugin&size=2756&sort=rating:desc',
     (result) => {
       let jsonResult = JSON.parse(result);
@@ -14,11 +18,32 @@ export default class HomePage extends Component {
     });
   }
 
+  setAddedPlugins() {
+    [].filter.call(this.gulpPluginsList, function (o) {
+      return o.selected;
+    }).map((o) => {
+      this.setState({addedPlugins: this.state.addedPlugins.concat(this.state.gulpPlugins.results.splice(o.value, 1))});
+      return true;
+    });
+  }
+
+  getAddedPlugins() {
+    if (this.state && this.state.addedPlugins) {
+      return (this.state.addedPlugins.map(
+        (result) => {
+          return <option>{result.name[0]}</option>;
+        }
+      ));
+    } else {
+      return <option>{'None'}</option>;
+    }
+  }
+
   getGulpPlugins() {
     if (this.state && this.state.gulpPlugins) {
       return (this.state.gulpPlugins.results.map(
-        (result) => {
-          return <option>{result.name[0]}</option>;
+        (result, index) => {
+          return <option value={index}>{result.name[0]}</option>;
         }
       ));
     } else {
@@ -30,8 +55,12 @@ export default class HomePage extends Component {
     return (
       <main>
         <h1>Testing</h1>
-        <select multiple>
+        <select ref={ (ref) => this.gulpPluginsList = ref } multiple>
           {this.getGulpPlugins()}
+        </select>
+        <Button onClick={this.setAddedPlugins.bind(this)}>Add Plugin</Button>
+        <select multiple>
+          {this.getAddedPlugins()}
         </select>
         <br />
         <NavButton to={''}>Back</NavButton>
