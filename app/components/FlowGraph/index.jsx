e16b1e608af26efcfc60109d9ec4afeb7e519240
Wyatt Arent <hello@wyatt.ninja>
2016-07-24 12:05:08 -0600

    Improve joint example

--- app/components/FlowGraph/index.jsx
+++ app/components/FlowGraph/index.jsx
@@ -11,33 +11,69 @@ export default class Graph extends Component {
         this.graph = new joint.dia.Graph();
     }
 
+    addTask({props, shape}) {
+      return new joint.shapes.basic[shape](props);
+    }
+
     componentDidMount() {
         this.paper = new joint.dia.Paper({
             el: findDOMNode(this.refs.placeholder),
             width: 600,
-            height: 200,
+            height: '100vh',
             model: this.graph,
-            gridSize: 1
+            gridSize: 4
         });
 
-        const rect = new joint.shapes.basic.Rect({
-            position: { x: 100, y: 30 },
-            size: { width: 100, height: 30 },
-            attrs: {
-                rect: { fill: 'blue' },
-                text: { text: 'my box', fill: 'white' }
-            }
+        const task1 = this.addTask({
+          shape: 'Rect',
+          props: {
+              position: { x: 100, y: 30 },
+              size: { width: 100, height: 30 },
+              attrs: {
+                  rect: { fill: 'green' },
+                  text: { text: 'Some Task', fill: 'white' }
+              }
+          }
         });
 
-        const rect2 = rect.clone();
-        rect2.translate(300);
-
-        const link = new joint.dia.Link({
-            source: { id: rect.id },
-            target: { id: rect2.id }
+        const task2 = this.addTask({
+          shape: 'Rect',
+          props: {
+              position: { x: 100, y: 30 },
+              size: { width: 100, height: 30 },
+              attrs: {
+                  rect: { fill: 'green' },
+                  text: { text: 'Other Task', fill: 'white' }
+              }
+          }
         });
 
-        this.graph.addCells([rect, rect2, link]);
+        const parallel = this.addTask({
+          shape: 'Circle',
+          props: {
+              position: { x: 100, y: 30 },
+              size: { width: 120, height: 120 },
+              attrs: {
+                  circle: { fill: 'blue' },
+                  text: { text: 'Some Parallel', fill: 'white' }
+              }
+          }
+        });
+
+        parallel.translate(100);
+        task2.translate(200);
+
+        const link1 = new joint.dia.Link({
+            source: { id: task1.id },
+            target: { id: parallel.id }
+        });
+
+        const link2 = new joint.dia.Link({
+            source: { id: task2.id },
+            target: { id: parallel.id }
+        });
+
+        this.graph.addCells([task1, task2, parallel, link1, link2]);
     }
 
     render() {
